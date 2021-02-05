558d4533c68938935f773ee4a19182dc67c8c702
Vadim Klimov <origi2012@mail.ru>
2021-02-05 22:54:25 +0300

    depth first traversal using recursion

--- graphs/depth_first_traversal_am_rec.h
+++ graphs/depth_first_traversal_am_rec.h
@@ -8,37 +8,6 @@
 *  Auxiliary space: O(V)                     *
 **********************************************/
 
-//C-style
-
-void visit_vertex(int cur_vert, int** graph, size_t size, bool* visited);
-
-void depth_first_traversal(int start, int** graph, size_t size)
-{
-	bool* visited = new bool[size];
-	memset(visited, 0, size);
-
-	visited[start] = true;
-
-	for (int adj_vert(0); adj_vert < size; ++adj_vert)
-	{
-		if (!visited[adj_vert] && graph[start][adj_vert])
-			visit_vertex(adj_vert, graph, size, visited);
-	}
-}
-
-void visit_vertex(int cur_vert, int** graph, size_t size, bool* visited)
-{
-	visited[cur_vert] = true;
-
-	for (int adj_vert(0); adj_vert < size; ++adj_vert)
-	{
-		if (!visited[adj_vert] && graph[cur_vert][adj_vert])
-			visit_vertex(adj_vert, graph, size, visited);
-	}
-}
-
-//STL-style
-
 void visit_vertex(int cur_vert, std::vector<std::vector<int>>& graph, std::vector<bool>& visited);
 
 void depth_first_traversal(int start, std::vector<std::vector<int>>& graph)
