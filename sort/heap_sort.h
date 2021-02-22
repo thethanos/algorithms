3a648ab5cf0c3da68eff2b42c0be105af173c5e0
thethanos <thethanosdaddy@gmail.com>
2021-02-22 14:29:03 +0300

    heap sort

--- sort/heap_sort.h
+++ sort/heap_sort.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <algorithm>
+
 /*********Heap sort**************
 *  Time complexity: O(nLog(n))  *
 *  Auxiliary space: O(1)        *
@@ -41,5 +43,54 @@ void heapify(size_t cur, T* data, size_t size)
 	{
 		std::swap(data[cur], data[max]);
 		heapify(max, data, size);
+	}
+}
+
+//STL-style
+
+template <typename RandomIt>
+void heapify(RandomIt begin, RandomIt end, RandomIt item);
+
+template <typename RandomIt>
+void heap_sort(RandomIt begin, RandomIt end)
+{
+	RandomIt iter = std::next(begin, std::distance(begin, end) / 2 - 1);
+
+	while (true)
+	{
+		heapify(begin, end, iter);
+		if (iter == begin) break;
+		--iter;
+	}
+	
+	for (auto iter(--end); iter != begin; --iter, --end)
+	{
+		std::iter_swap(begin, iter);
+		heapify(begin, end, begin);
+	}
+}
+
+template <typename RandomIt>
+void heapify(RandomIt begin, RandomIt end, RandomIt item)
+{
+	size_t cur_index = 2 * std::distance(begin, item);
+	RandomIt max = item, left = end, right = end;
+
+	if(std::distance(begin, end) > cur_index + 1)
+		left  = std::next(begin, cur_index + 1);
+
+	if(std::distance(begin, end) > cur_index + 2)
+		right = std::next(begin, cur_index + 2);
+
+	if (left < end && *item < *left)
+		max = left;
+
+	if (right < end && *max < *right)
+		max = right;
+
+	if (max != item)
+	{
+		std::iter_swap(item, max);
+		heapify(begin, end, max);
 	}
 }
