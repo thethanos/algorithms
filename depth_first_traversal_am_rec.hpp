9468ab4f28e5ec9391a7676ff22d0a51898c0b35
thethanos <51026816+thethanos@users.noreply.github.com>
2021-09-19 15:34:09 +0300

    Topological sort and new header file extensions

---
+++ depth_first_traversal_am_rec.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <vector>
+
+using std::vector;
+
+/******Depth first traversal (recursion)******
+*  Graph representation: adjacency matrix    * 
+*  Time complexity: O(V * V)                 *
+*  Auxiliary space: O(V)                     *
+**********************************************/
+
+void visit_vertex(int cur_vert, const vector<vector<int>>& graph, vector<bool>& visited);
+
+void depth_first_traversal(int start, const vector<vector<int>>& graph)
+{
+	vector<bool> visited(graph.size());
+
+	visited[start] = true;
+
+	for (int adj_vert(0); adj_vert < graph.size(); ++adj_vert)
+	{
+		if (!visited[adj_vert] && graph[start][adj_vert])
+			visit_vertex(adj_vert, graph, visited);
+	}
+}
+
+void visit_vertex(int cur_vert, const vector<vector<int>>& graph, vector<bool>& visited)
+{
+	visited[cur_vert] = true;
+
+	for (int adj_vert(0); adj_vert < graph.size(); ++adj_vert)
+	{
+		if (!visited[adj_vert] && graph[cur_vert][adj_vert])
+			visit_vertex(adj_vert, graph, visited);
+	}
+}
